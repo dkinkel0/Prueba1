29cc9244ce58bab4e33fa744a736837a2e99272e
dkinkel0 <darioekinkela@gmail.com>
2022-08-09 19:48:14 -0300

    Preparando para deployar

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export function getAllOneGames(name) {
     if (name) {
         return async function (dispatch) {
-            let json = await axios.get(`http://localhost:3001/videogames?name=${name}`);
+            let json = await axios.get(`/videogames?name=${name}`);
             return dispatch({
                 type: 'GET_ALL_ONE_GAMES',
                 payload: json.data
@@ -11,7 +11,7 @@ export function getAllOneGames(name) {
         }
     } else {
         return async function (dispatch) {
-            let json = await axios.get(`http://localhost:3001/videogames`);
+            let json = await axios.get(`/videogames`);
             return dispatch({
                 type: 'GET_ALL_ONE_GAMES',
                 payload: json.data
@@ -23,7 +23,7 @@ export function getAllOneGames(name) {
 export function getIdGame(id) {
 
     return async function (dispatch) {
-        let json = await axios.get(`http://localhost:3001/videogame/${id}`);
+        let json = await axios.get(`/videogame/${id}`);
         return dispatch({
             type: 'GET_ID_GAME',
             payload: json.data
@@ -34,7 +34,7 @@ export function getIdGame(id) {
 export function getGenres() {
 
     return async function (dispatch) {
-        let json = await axios.get(`http://localhost:3001/genres`);
+        let json = await axios.get(`/genres`);
         return dispatch({
             type: 'GET_GENRES',
             payload: json.data
@@ -45,7 +45,7 @@ export function getGenres() {
 export function getPlatforms() {
 
     return async function (dispatch) {
-        let json = await axios.get(`http://localhost:3001/platforms`);
+        let json = await axios.get(`/platforms`);
         return dispatch({
             type: 'GET_PLATFORMS',
             payload: json.data
@@ -56,7 +56,7 @@ export function getPlatforms() {
 export function createGame(input) {
    
     return async function (dispatch) {
-        let json = await axios.post('http://localhost:3001/videogames', input);
+        let json = await axios.post('/videogames', input);
         return dispatch({
             type: 'CREATE_GAME',
             payload: json.data
