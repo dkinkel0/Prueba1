b5c24e29caba19e6a56d200b29009032b92b8260
dkinkel0 <darioekinkela@gmail.com>
2022-08-09 20:32:24 -0300

    agregue nodemon

--- api/src/db.js
+++ api/src/db.js
@@ -69,3 +69,5 @@ module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
+
+// https://github.com/timanovsky/subdir-heroku-biuldpack
